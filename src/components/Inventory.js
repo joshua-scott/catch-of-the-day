6f8f9181559f35333acb89c761d21f2114e96246
joshua-scott <jscott313@gmail.com>
2018-07-05 23:13:11 +0300

    End of video 6

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Inventory extends React.Component {
+  render() {
+    return <div className="inventory">Inventory component!</div>
+  }
+}
+
+export default Inventory
