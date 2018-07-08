2a52d2f7dc376590f9a8ecad733d982472eb49c9
Joshua Scott <jscott313@gmail.com>
2018-07-08 21:29:28 +0300

    Add AddFishForm, use it to setState on App

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef()
+  priceRef = React.createRef()
+  statusRef = React.createRef()
+  descRef = React.createRef()
+  imageRef = React.createRef()
+
+  createFish = e => {
+    e.preventDefault()
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    }
+    this.props.addFish(fish)
+    e.currentTarget.reset()
+  }
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" placeholder="Name" ref={this.nameRef} />
+        <input
+          name="price"
+          type="text"
+          placeholder="Price"
+          ref={this.priceRef}
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" placeholder="Desc" ref={this.descRef} />
+        <input
+          name="image"
+          type="text"
+          placeholder="Image"
+          ref={this.imageRef}
+        />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm
